2eb546a9b282037501b7fff962516fd0f97f9efb
Marko Bakovic <delta003@users.noreply.github.com>
2023-09-07 17:17:35 +0200

    Add more implementations using Pydust functionality (#3)

--- src/fib.zig
+++ src/fib.zig
@@ -1,13 +1,84 @@
 const std = @import("std");
 const py = @import("pydust");
 
-pub fn get_fibonacci(args: struct { n: py.PyLong }) !u64 {
-    return f(try args.n.as(u64));
+// A simple fibonacci implementation.
+pub fn nth_fibonacci_iterative(args: struct { n: u64 }) u64 {
+    if (args.n < 2) return args.n;
+    var f0: u64 = 0;
+    var f1: u64 = 1;
+    var fnth: u64 = f0 + f1;
+    for (2..args.n) |_| {
+        f0 = f1;
+        f1 = fnth;
+        fnth = f0 + f1;
+    }
+    return fnth;
 }
 
-fn f(n: u64) u64 {
-    return if (n < 2) n else f(n - 1) + f(n - 2);
+// A simple recursive fibonacci implementation.
+pub fn nth_fibonacci_recursive(args: struct { n: u64, f0: u64 = 0, f1: u64 = 1 }) u64 {
+    if (args.n == 0) return args.f0;
+    if (args.n == 1) return args.f1;
+    return nth_fibonacci_recursive(.{
+        .n = args.n - 1,
+        .f0 = args.f1,
+        .f1 = args.f0 + args.f1,
+    });
 }
+
+// Leveraging the `@call` function to enforce a tail call.
+pub fn nth_fibonacci_recursive_tail(args: struct { n: u64 }) u64 {
+    return fibonacci_recursive_tail(args.n, 0, 1);
+}
+fn fibonacci_recursive_tail(n: u64, f0: u64, f1: u64) u64 {
+    if (n == 0) return f0;
+    if (n == 1) return f1;
+    return @call(.always_tail, fibonacci_recursive_tail, .{ n - 1, f1, f0 + f1 });
+}
+
+// Exposing it through a Python class.
+pub const Fibonacci = py.class("Fibonacci", struct {
+    pub const __doc__ = "A class that computes the Fibonacci numbers.";
+
+    const Self = @This();
+
+    first_n: u64,
+
+    pub fn __new__(args: struct { first_n: u64 }) !Self {
+        return .{ .first_n = args.first_n };
+    }
+
+    // Get an iterator over the first `self.first_n` Fibonacci numbers.
+    pub fn __iter__(self: *const Self) !*FibonacciIterator {
+        return try py.init(FibonacciIterator, .{ .i = 0, .ith = 0, .next = 1, .stop = self.first_n });
+    }
+});
+
+pub const FibonacciIterator = py.class("FibonacciIterator", struct {
+    pub const __doc__ = "An iterator that computes the Fibonacci numbers.";
+
+    const Self = @This();
+
+    i: u64,
+    ith: u64,
+    next: u64,
+    stop: u64,
+
+    pub fn __new__(args: struct { i: u64, ith: u64, next: u64, stop: u64 }) !Self {
+        return .{ .i = args.i, .ith = args.ith, .next = args.next, .stop = args.stop };
+    }
+
+    pub fn __next__(self: *Self) !?u64 {
+        // Stop iteration when we reach `self.stop`.
+        if (self.i == self.stop) return null;
+        defer self.i += 1;
+
+        const result = self.ith;
+        self.ith = self.next;
+        self.next = result + self.ith;
+        return result;
+    }
+});
 
 comptime {
     py.module(@This());
@@ -20,15 +91,23 @@ comptime {
 
 const testing = std.testing;
 
-test "fibonacci test" {
+test "fibonacci iterative" {
     py.initialize();
     defer py.finalize();
 
-    const pl = try py.PyLong.from(u64, 9);
-    defer pl.decref();
+    try testing.expectEqual(@as(u64, 34), nth_fibonacci_iterative(.{ .n = 9 }));
+}
 
-    try testing.expectEqual(
-        @as(u64, 34),
-        try get_fibonacci(.{ .n = pl }),
-    );
+test "fibonacci recursive" {
+    py.initialize();
+    defer py.finalize();
+
+    try testing.expectEqual(@as(u64, 34), nth_fibonacci_recursive(.{ .n = 9 }));
+}
+
+test "fibonacci recursive tail" {
+    py.initialize();
+    defer py.finalize();
+
+    try testing.expectEqual(@as(u64, 34), nth_fibonacci_recursive_tail(.{ .n = 9 }));
 }
--- test/test_fib.py
+++ test/test_fib.py
@@ -2,4 +2,26 @@ from fibonacci import _lib
 
 
 def test_fibonacci():
-    assert _lib.get_fibonacci(9) == 34
+    impls = [
+        _lib.nth_fibonacci_iterative,
+        _lib.nth_fibonacci_recursive,
+        _lib.nth_fibonacci_recursive_tail,
+    ]
+    for impl in impls:
+        assert impl(9) == 34
+
+
+def test_fubonacci_iterator():
+    fibonacci = _lib.Fibonacci(10)
+    expected = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+    # As iterator
+    fibonacci_iter = iter(fibonacci)
+    for expected_item in expected:
+        actual = next(fibonacci_iter)
+        assert actual == expected_item
+
+    # As list
+    fibonacci_list = list(fibonacci)
+    for actual, expected_item in zip(fibonacci_list, expected):
+        assert actual == expected_item
