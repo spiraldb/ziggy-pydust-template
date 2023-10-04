2c9597933fb4a5bbbcd22796d31f307728d0c59a
Robert Kruszewski <github@robertk.io>
2023-10-04 13:44:42 +0100

    Upgrade ziggy-pydust to 0.12.3 (#27)
    Accounts for changes in stub generation where stubs need to be checked
    in

---
+++ fibonacci/_lib.pyi
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+def nth_fibonacci_iterative(n, /): ...
+def nth_fibonacci_recursive(n, /, *, f0=0, f1=1): ...
+def nth_fibonacci_recursive_tail(n, /): ...
+
+class Fibonacci:
+    """
+    A class that computes the Fibonacci numbers.
+    """
+
+    def __init__(first_n, /):
+        pass
+    def __iter__(self, /):
+        """
+        Implement iter(self).
+        """
+        ...
+
+class FibonacciIterator:
+    """
+    An iterator that computes the Fibonacci numbers.
+    """
+
+    def __init__(i, ith, next, stop, /):
+        pass
+    def __next__(self, /):
+        """
+        Implement next(self).
+        """
+        ...
