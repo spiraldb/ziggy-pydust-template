28b565dd0118c4b7c57641f9e0005fd72c4ffd08
Robert Kruszewski <github@robertk.io>
2025-06-11 15:29:16 -0700

    bump

--- fibonacci/_lib.pyi
+++ fibonacci/_lib.pyi
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 def nth_fibonacci_iterative(n, /): ...
 def nth_fibonacci_recursive(n, /, *, f0=0, f1=1): ...
 def nth_fibonacci_recursive_tail(n, /): ...
@@ -9,23 +7,19 @@ class Fibonacci:
     A class that computes the Fibonacci numbers.
     """
 
-    def __init__(self, first_n, /):
-        pass
+    def __init__(self, first_n, /) -> None: ...
     def __iter__(self, /):
         """
         Implement iter(self).
         """
-        ...
 
 class FibonacciIterator:
     """
     An iterator that computes the Fibonacci numbers.
     """
 
-    def __init__(self, i, ith, next, stop, /):
-        pass
+    def __init__(self, i, ith, next, stop, /) -> None: ...
     def __next__(self, /):
         """
         Implement next(self).
         """
-        ...
